296d92750f4f1b09d266ed72f43ce2f918c98cb3
hmdnubaidillah <daniuyan71@gmail.com>
2023-12-08 21:04:40 +0700

    change font to montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
 });
@@ -9,7 +9,7 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
