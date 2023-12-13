fe7cd1d7494f9e4808a77c4d56cd321328ddd295
hmdnubaidillah <daniuyan71@gmail.com>
2023-12-13 09:19:48 +0700

    merging: carousel-homepage, article-page

--- constant/index.ts
+++ constant/index.ts
@@ -1,8 +1,5 @@
-import { CarouselHome } from "@/types";
-
-import carousel2 from "@/public/images/carousel2.png";
-import profile from "@/public/images/profile.png";
-import { ArticleContentProps } from "@/types";
+import { CarouselHome, ArticleContentProps } from "@/types";
+import { profile, carousel2 } from "@/public";
 
 export const navLinks = [
   {
@@ -25,15 +22,40 @@ export const navLinks = [
 
 export const carouselHome: CarouselHome[] = [
   {
-    carousel: "carousel1",
+    carousel: "bg-carousel1",
     heading: "Ini Heading Tolong Jangan Hujat Saya Karena Tulisan “Saksikan” yang Sebelumnya",
     paragraph:
       "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
   },
   {
-    carousel: "carousel2",
+    carousel: "bg-carousel2",
     heading: "Ini Heading Tolong Jangan Hujat Saya Karena Tulisan “Saksikan” yang Sebelumnya",
     paragraph:
       "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  },
+];
+
+export const articleContents: ArticleContentProps[] = [
+  {
+    imgAuthor: profile,
+    author: "John Doe",
+    date: "• Agustus 2023",
+    category: "Kesenian",
+    title: "Sejarah Pertunjukan Reog Ponorogo dikalangan Masyarakat Jawa Timur",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt placeat quod voluptates, laborum magni ipsum cupiditate aliquid totam aut optio incidunt expedita debitis libero, consequuntur eligendi odit adipisci dolorum officiis.",
+    img: carousel2,
+    imgAlt: "karapan-sapi",
+  },
+  {
+    imgAuthor: profile,
+    author: "Lavina",
+    date: "• Agustus 2023",
+    category: "Kuliner",
+    title: "Daya Tarik Kesenian Bantengan Terhadap Masyarakat di Berbagai Kalangan Usia",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt placeat quod voluptates, laborum magni ipsum cupiditate aliquid totam aut optio incidunt expedita debitis libero, consequuntur eligendi odit adipisci dolorum officiis.",
+    img: carousel2,
+    imgAlt: "karapan-sapi",
   },
 ];
