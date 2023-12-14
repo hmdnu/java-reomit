1f41adab212c9b76e8912d02a40f8530137bc113
hmdnubaidillah <daniuyan71@gmail.com>
2023-12-14 21:42:49 +0700

    refactor: moving quote to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { CarouselHome, CategoryCulture, ArticleCardHome } from "@/components";
+import { CarouselHome, CategoryCulture, ArticleCardHome, QuoteHomepage } from "@/components";
 import { stats } from "@/constant";
-import { quote } from "@/public";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -20,37 +18,8 @@ export default function Home() {
           ))}
         </div>
       </section>
-
-      {/* category */}
       <CategoryCulture />
-
-      {/* quote */}
-      <section className="w-full h-screen">
-        <div className="relative">
-          <div className="absolute h-[85vh] w-full bg-carousel1 bg-cover bg-no-repeat invert rotate-180 -scale-x-100"></div>
-          <div className="absolute h-[85vh] w-full flex justify-start items-center">
-            <div className="w-[80%] m-auto">
-              <div className="relative w-fit px-12 py-7">
-                <Image src={quote} width={40} height={40} alt="quote" className="absolute top-0 left-0" />
-                <span>
-                  <h1 className="text-h1 font-bold text-white w-[500px] my-3">
-                    Esensi dari kebudayaan adalah Menghormati
-                  </h1>
-                </span>
-                <Image
-                  src={quote}
-                  width={40}
-                  height={40}
-                  alt="quote"
-                  className="rotate-180 absolute bottom-0 right-0"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* article card */}
+      <QuoteHomepage />
       <ArticleCardHome />
     </main>
   );
--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,6 @@ import CarouselHome from "./CarouselHome";
 import Nav from "./Nav";
 import CategoryCulture from "./CategoryCulture";
 import ArticleCardHome from "./ArticleCardHome";
+import QuoteHomepage from "./QuoteHomepage";
 
-export { ArticleCard, CarouselHome, Nav, CategoryCulture, ArticleCardHome };
+export { ArticleCard, CarouselHome, Nav, CategoryCulture, ArticleCardHome, QuoteHomepage };
