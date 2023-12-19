9c41d7e2920c56e3ff773460fde98161bbca21ee
Lavina-23 <lavinakohai3227@gmail.com>
2023-12-19 22:11:23 +0700

    feat: slicing detail budaya *maafkan aku dan :(

---
+++ app/daftar-budaya/detail/page.tsx
@@ -0,0 +1,9 @@
+import DetailBudaya from "@/components/daftarBudayaPage/DetailBudaya";
+
+export default function Page() {
+  return (
+    <main className="mt-24">
+      <DetailBudaya />
+    </main>
+  );
+}
