8829bffd88e2202bcc192e7dcc2683281fc69bb9
Lavina-23 <lavinakohai3227@gmail.com>
2023-12-22 10:49:21 +0700

    fix/desktop event responsive

--- app/kegiatan/page.tsx
+++ app/kegiatan/page.tsx
@@ -10,7 +10,7 @@ export default function page() {
   return (
     <section className="mt-24 md:mt-32 w-full md:w-[75%] m-auto">
       <div>
-        <div className="text-fontPrimary m-auto mx-6 sm:mx-16 sm:justify-between md:justify-start flex md:gap-[32px] items-center">
+        <div className="text-fontPrimary m-auto mx-6 sm:mx-16 md:mx-0 sm:justify-between md:justify-start flex md:gap-[32px] items-center">
           <h1 className="hidden sm:text-h2 font-bold ">Event</h1>
           <SearchBar />
 
@@ -22,7 +22,7 @@ export default function page() {
         </div>
 
         {/* event card */}
-        <div className="grid place-items-center sm:mx-16 my-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[50px]">
+        <div className="grid place-items-center sm:mx-16 md:mx-0 my-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[50px]">
           {eventCard.map((event, i) => (
             <EventCard key={i} event={event} />
           ))}
