7bf828ba1a3d1842a5dcc0e2ee0cd758e0644915
Lavina-23 <lavinakohai3227@gmail.com>
2023-12-22 15:14:23 +0700

    feat: mobile and tab register responsive

--- app/registrasi/page.tsx
+++ app/registrasi/page.tsx
@@ -1,8 +1,12 @@
 import { SignInCard } from "@/components";
 
+export const metadata = {
+  title: "Registrasi",
+};
+
 export default function Page() {
   return (
-    <main className="main-layout">
+    <main className="sm:mx-10 md:main-layout">
       <SignInCard />
     </main>
   );
